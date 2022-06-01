d7064d15347e4158475809c3e906401bac7e44a1
Ziad ISMAILI ALAOUI <94684889+ismaili-ziad@users.noreply.github.com>
2022-06-01 16:12:14 +0100

    Update problem001.hs

--- Solutions/problem001.hs
+++ Solutions/problem001.hs
@@ -5,4 +5,4 @@ l = [x | x <- [1..999], mod x 3 == 0 || mod x 5 == 0]
 answer = sum l
 
 -- Explanation:
--- 'answer' sums all elements in the list 'l' containg all integers from 1 to 999 (inclusive) either divisble by 3 or 5.
+-- 'answer' sums all elements in the list 'l' containing all integers from 1 to 999 (inclusive) either divisible by 3 or by 5.
